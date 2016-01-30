704675f6e56d473c8d6cb221ca2e4ec9393f6205
centos <784378814@qq.com>
2016-01-30 11:11:52 +0800

    write group.py

--- system/basesystem/basedirection.py
+++ system/basesystem/basedirection.py
@@ -6,6 +6,8 @@ import commands
 
 import os
 
+from samba.system.basesystem.baseuser import docmd, template
+
 __author__ = 'bary'
 __metaclass__ = type
 
@@ -19,15 +21,18 @@ log = logger.getLogger('logger.basedirection')
 info = r"ls -ld {{ path }}"
 chgrp = r"chgrp -R {{ groupname }} {{ path }}"
 chown = r"chown -R {{ username }} {{ path }}"
-chmod = r"chmod -R u={{ u }} g={{ g }} o={{ o }} {{ path }}"
-setaclu = r"setacl -mRd u:{{ user }}:{{ u }} {{ path }}"
-setaclo = r"setacl -mRd u::{{ u }} {{ path }}"
-setaclg = r"setacl -mRd g:{{ group }}:{{ u }} {{ path }}"
-setaclm = r"setacl -mRd m:{{ u }} {{ path }}"
-getacl = r"getacl {{ path }}"
+chmod = r"chmod -R {% if u == "" %} u={{ u }} {% endif %}{%if g == "" %} g={{ g }} {% endif %}\
+{% if o == "" %} o={{ o }} {% endif %} {{ path }}"
+mkdir = r"mkdir {{ path }}"
+setaclu = r"setfacl -mRd u:{{ user }}:{{ u }} {{ path }}"
+setaclo = r"setfacl -mRd u::{{ u }} {{ path }}"
+setaclg = r"setfacl -mRd g:{{ group }}:{{ u }} {{ path }}"
+setaclm = r"setfacl -mRd m:{{ u }} {{ path }}"
+getacl = r"getfacl {{ path }}"
 
 
 class BaseDirection:
+    """
     def __init__(self, path=""):
         self.path = str(path)
         self.jurisdiction = ""
@@ -63,12 +68,65 @@ class BaseDirection:
             log.error(error)
             return -1
         return 0
+        """
 
-    def getall(self):
+    def __init__(self):
         pass
 
-    def changejurisdiction(self):
-        pass
+    def _getinfo(self, path):
+        cmd = template(info, path=path)
+        err, att = commands.getstatusoutput(cmd)
+        if err != 0:
+            error = "A error arise go get '" + path + "' info"
+            log.error(error)
+            return {"jurisdiction": "",
+                    "hardlinkorsub": "",
+                    "owner": "",
+                    "group": "",
+                    "size": "",
+                    "changetime": "",
+                    }
+        att = att.split(" ")
+        if "d" not in att[0]:
+            warning = "the path you input is not a direction"
+            log.warning(warning)
+            return {"jurisdiction": "",
+                    "hardlinkorsub": "",
+                    "owner": "",
+                    "group": "",
+                    "size": "",
+                    "changetime": "",
+                    }
+        try:
+            jurisdiction = att[0]
+            hardlinkorsub = int(att[1])
+            owner = att[2]
+            group = att[3]
+            size = int(att[4])
+            changetime = (att[5], att[6], att[7])
+        except Exception as e:
+            error = "A error arise when get attribute of dir " + path + str(e)
+            log.error(error)
+            return {"jurisdiction": "",
+                    "hardlinkorsub": "",
+                    "owner": "",
+                    "group": "",
+                    "size": "",
+                    "changetime": "",
+                    }
+        return {"jurisdiction": jurisdiction,
+                "hardlinkorsub": hardlinkorsub,
+                "owner": owner,
+                "group": group,
+                "size": size,
+                "changetime": changetime
+                }
+
+    def getall(self, path=""):
+        print self._getinfo(path=path)
+
+    def changejurisdiction(self, u="", g="", o="", path=""):
+        return docmd(chmod, u=u, g=g, o=o, path=path)
 
     def createsub(self):
         pass
@@ -114,14 +172,3 @@ class BaseDirection:
 
     def cpdir(self):
         pass
-
-
-
-
-
-
-
-
-
-
-
